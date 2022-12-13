48db0819f84183707e6594551394b566af3e8451
YouYou94 <ybh4115@gmail.com>
2022-12-13 15:43:48 +0900

    타입 추론

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var num1 = 10;
-var num2 = 20;
-function Add(x, y) {
-    return x + y;
-}
-console.log(Add(num1, num2));
+var a = 5;
+var b = '3';
+console.log(a - b);
+// 자바 스크립트 같은 경우 정적 타이핑으로 인해 2값이 나온다.
+// 그러나 타입 스크립트에서는 b 경우 string 값이기 때문에 에러가 난다.
+a = 7;
+b = 5; // b 같은 경우 string 값이기 때문에 number 값이 들어갈 수 없다.
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-let num1:number = 10;
-let num2:number = 20;
+let a = 5;
+let b = '3';
 
-function Add(x:number, y:number):number {
-    return x + y;
-}
+console.log(a - b);
+// 자바 스크립트 같은 경우 정적 타이핑으로 인해 2값이 나온다.
+// 그러나 타입 스크립트에서는 b 경우 string 값이기 때문에 에러가 난다.
 
-console.log(Add(num1,num2));
+a = 7;
+b = 5; // b 같은 경우 string 값이기 때문에 number 값이 들어갈 수 없다.
